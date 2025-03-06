90a4aa0425b17f50aff10db97b8b3e150490b07e
yanghuan <yang@xway.cn>
2025-03-06 17:55:10 +0800

    1

--- src/controllers/RecipeController.php
+++ src/controllers/RecipeController.php
@@ -74,8 +74,9 @@ class RecipeController extends BaseController
         if (parent::beforeAction($action)) {
             $userInfo = $this->getLoginUser();
             if (empty($userInfo) && in_array($action->id, $this->requireLoginActions)) {
-                return Util::jsonReturn(ResponseCode::LOGIN_REQUIRED,'no login');
+                Util::jsonReturn(ResponseCode::LOGIN_REQUIRED,'no login');
                 Yii::$app->end();
+                return false;
                 //throw new UserException('登录状态失效base', ResponseCode::LOGIN_REQUIRED);
             }
             return true;
